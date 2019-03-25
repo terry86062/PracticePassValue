6ccb7590f4e450e53ae7270923811d8d0ab2652f
Terry <terry86062@gmail.com>
2019-03-25 13:39:49 +0800

    Initial Commit

---
+++ PracticePassValue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PracticePassValue
+//
+//  Created by 黃偉勛 Terry on 2019/3/25.
+//  Copyright © 2019 Terry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
